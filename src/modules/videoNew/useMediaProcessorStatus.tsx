0bb96fb951fbb21a67342fd7c4202477c3a67da8
Tore Sinding Bekkedal <toresbe@protonmail.com>
2023-07-27 03:49:37 +0200

    Clean up encode status code

--- src/modules/videoNew/useMediaProcessorStatus.tsx
+++ src/modules/videoNew/useMediaProcessorStatus.tsx
@@ -1,40 +1,32 @@
 import getConfig from "next/config"
 import { useEffect, useState } from "react"
+import { useTimeout } from "usehooks-ts"
 
-// TODO: Replace hackish sleep with a proper solution
+// TODO: Replace hackish sleep with a proper solution and rely less on callbacks
 export const useMediaProcessorStatus = (
   uploadId: string,
   onComplete: () => void,
   onMediaId: (mediaId: number) => void,
 ) => {
-  const { FK_MEDIAPROC } = getConfig().publicRuntimeConfig
   const [percent, setPercent] = useState<number>(0)
   const [status, setStatus] = useState<"pending" | "connected" | "error">("pending")
+  // sleep 1 second to allow the job to be created (yeah, it's a hack...)
+  const [skip, setSkip] = useState<boolean>(true)
+  useTimeout(() => setSkip(false), 500)
+
   useEffect(() => {
-    // sleep 1 second to allow the job to be created (yeah, it's a hack...)
-    new Promise((resolve) => setTimeout(resolve, 1000)).then(() => {
-      const source = new EventSource(`${FK_MEDIAPROC}/status/${uploadId}`, { withCredentials: true })
-      source.onopen = (e) => {
-        setStatus("connected")
-      }
-      source.addEventListener("progress", (e) => {
-        const { percent } = JSON.parse(e.data)
-        setPercent(percent)
-      })
-      source.addEventListener("status", (e) => {
-        const { mediaId } = JSON.parse(e.data)
-        onMediaId(mediaId)
-      })
-      source.addEventListener("completed", (e) => {
-        onComplete()
-      })
-      source.onmessage = console.warn
-      source.onerror = (e) => {
-        console.log(e)
-      }
-      return () => source.close()
-    })
-  }, [])
+    if (skip) return
+
+    const { FK_MEDIAPROC } = getConfig().publicRuntimeConfig
+    const source = new EventSource(`${FK_MEDIAPROC}/status/${uploadId}`, { withCredentials: true })
+    source.onopen = () => setStatus("connected")
+    source.onerror = console.log
+    source.addEventListener("progress", ({ data }) => setPercent(JSON.parse(data).percent))
+    source.addEventListener("status", ({ data }) => onMediaId(JSON.parse(data).mediaId))
+    source.addEventListener("completed", onComplete)
+
+    return () => source.close()
+  }, [skip, setPercent, onMediaId, onComplete, uploadId])
 
   return { percent, status }
 }
