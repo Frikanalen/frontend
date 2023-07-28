717c5fed14b013d755036445e3488777fe711764
Tore Sinding Bekkedal <toresbe@gmail.com>
2023-07-28 03:17:25 +0200

    Add "use client" to some files
    maybe that changes something

--- src/modules/videoNew/useMediaProcessorStatus.tsx
+++ src/modules/videoNew/useMediaProcessorStatus.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useEffect, useState, useRef } from "react"
 
 export const useMediaProcessorStatus = (
--- src/modules/videoNew/videoCreationUpload.tsx
+++ src/modules/videoNew/videoCreationUpload.tsx
@@ -1,3 +1,4 @@
+"use client"
 import * as Progress from "@radix-ui/react-progress"
 import React, { useCallback, useEffect, useMemo, useState } from "react"
 import { useCookie } from "react-use"
