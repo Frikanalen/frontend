64300948c7be8ca8e8fe33acc41415d46dae76f7
Tore Sinding Bekkedal <toresbe@protonmail.com>
2025-07-10 01:22:33 +0200

    Start på en daypicker for schedule

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,7 +1,22 @@
 "use client";
 
+import { DayPicker } from "react-day-picker";
+import { nb } from "react-day-picker/locale";
+import "react-day-picker/style.css";
+import "./date-picker.css";
+import { useState } from "react";
 
 export default function Schedule() {
-    return (<section className="flex w-full flex-col items-center justify-center gap-4 py-8 md:py-10">
-    </section>)
+  const [selected, setSelected] = useState<Date>();
+  return (
+    <section className="flex w-full flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <DayPicker
+        animate
+        mode={"single"}
+        locale={nb}
+        selected={selected}
+        onSelect={setSelected}
+      />
+    </section>
+  );
 }
