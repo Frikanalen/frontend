8cb6c4f3b892c78d8baeaa7c4c18fc8559da98ad
Tore Sinding Bekkedal <toresbe@protonmail.com>
2025-07-09 18:41:18 +0200

    oppdaterer footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,12 +3,11 @@
 import { Link } from "@heroui/react";
 
 export const Footer = () => (
-  <footer className={"pt-4 pb-2 flex justify-between max-w-3xl w-full px-4"}>
+  <footer className={"pt-10 pb-9 flex justify-between max-w-3xl w-full px-4"}>
     <div>© 2009 - {new Date().getFullYear()} Foreningen Frikanalen</div>
-    <div className="flex gap-2">
-      <Link href="https://github.com/Frikanalen/frikanalen">GitHub</Link>
+    <div className="flex gap-4">
+      <Link href="https://github.com/Frikanalen">GitHub</Link>
       <Link href="https://frikanalen.no/api/">API</Link>
-      <Link href="https://frikanalen.no/xmltv/">XMLTV</Link>
     </div>
   </footer>
 );
