31477780be8e5bae648d05998ef8cf5734b03cc9
Tore Sinding Bekkedal <toresbe@protonmail.com>
2025-07-09 18:48:00 +0200

    fix: no classnames in layout.tsx

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 
 import { Card, CardBody, CardHeader } from "@heroui/react";
-import { Layout } from "@/app/about/layout";
 
 export default function Register() {
   return (
-    <Layout>
-      <Card>
-        <CardHeader>Registrering</CardHeader>
-        <CardBody>Hello</CardBody>
-      </Card>
-    </Layout>
+    <Card>
+      <CardHeader>Registrering</CardHeader>
+      <CardBody>Hello</CardBody>
+    </Card>
   );
 }
