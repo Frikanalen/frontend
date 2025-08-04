65c1b1a4c5f90ab25086664aa0693a144394f8dd
Tore Sinding Bekkedal <toresbe@protonmail.com>
2025-08-04 21:30:54 +0200

    next: add rewrites to API in dev mode

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8000/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
