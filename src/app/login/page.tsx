fcfcc3f0279d64d7d993fe9727b946d154cac2ef
Tore Sinding Bekkedal <toresbe@protonmail.com>
2025-08-04 21:31:45 +0200

    login: work on login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,50 +1,58 @@
 "use client";
 import { Button, Form, Input } from "@heroui/react";
 import Link from "next/link";
-import { useActionState } from "react";
-import { obtainTokenCreate } from "@/generated/obtain-token/obtain-token";
+import z from "zod";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useUserLoginCreate } from "@/generated/user/user";
+import { useRouter } from "next/navigation";
 
-type State = { error?: string };
-
-type UserLoginForm = {
-  username: string;
-  password: string;
-};
+const UserLoginFormSchema = z.object({
+  username: z.string(),
+  password: z.string(),
+});
 
 export default function Login() {
-  const [{ error }, formAction] = useActionState<State, UserLoginForm>(
-    async (state: State, payload: UserLoginForm): Promise<State> => {
-      const token = await obtainTokenCreate({
-        username: payload.username,
-        password: payload.password,
-      });
-      return { ...state, error: token.data.token };
-    },
-    {
-      error: "foo",
-    },
-  );
+  const { control, handleSubmit } = useForm({
+    resolver: zodResolver(UserLoginFormSchema),
+  });
+  const router = useRouter();
+  const { mutateAsync } = useUserLoginCreate();
 
   return (
     <div className={"grid-cols-2 grid gap-4"}>
       <div className={"space-y-4"}>
         <h2 className={"text-lg font-bold"}>Logg inn</h2>
-        <Form action={formAction}>
-          <Input
-            isRequired
-            name="username"
-            label="E-post"
-            type="email"
-            autoComplete={"username"}
+        <Form
+          onSubmit={handleSubmit(async (data) =>
+            mutateAsync({ data }).then(() => router.push("/profile")),
+          )}
+        >
+          <Controller
+            control={control}
+            name={"username"}
+            render={({ field }) => (
+              <Input
+                isRequired
+                label="E-post"
+                type="email"
+                autoComplete={"username"}
+                {...field}
+              />
+            )}
           />
-          <Input
-            isRequired
-            name="password"
-            label="Passord"
-            type="password"
-            autoComplete={"password"}
+          <Controller
+            control={control}
+            name={"password"}
+            render={({ field }) => (
+              <Input
+                label="Passord"
+                type="password"
+                autoComplete={"password"}
+                {...field}
+              />
+            )}
           />
-          <pre>{JSON.stringify(error, null, 2)}</pre>
           <Button className={"ml-auto"} type="submit">
             Logg inn
           </Button>
