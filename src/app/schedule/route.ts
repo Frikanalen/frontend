26df3dff02d44ba0d1a448046f9fb12f36893a92
Tore Sinding Bekkedal <toresbe@protonmail.com>
2025-08-07 01:54:15 +0200

    bugfix: fix incorrect redirect

--- src/app/schedule/route.ts
+++ src/app/schedule/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export const runtime = "edge";
 
 // Redirect to today's schedule, in [year]/[month]/[date]/page.tsx.
-export function GET(req: Request) {
+export function GET() {
   const [year, month, day] = new Date().toISOString().slice(0, 10).split("-");
-  return NextResponse.redirect(req.url + `/${year}/${month}/${day}`);
+  return NextResponse.redirect(`/${year}/${month}/${day}`);
 }
