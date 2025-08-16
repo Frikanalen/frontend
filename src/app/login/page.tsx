c43d1ed1e85708178ece59bc9a3b0fa6edcef627
Tore Sinding Bekkedal <toresbe@protonmail.com>
2025-08-16 13:03:57 +0200

    Login page: fix design, refactor

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,6 +7,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useUserLoginCreate } from "@/generated/user/user";
 import { useState } from "react";
 import { isAxiosError } from "axios";
+import type { LoginRequest } from "@/generated/frikanalenDjangoAPI.schemas";
 
 const UserLoginFormSchema = z.object({
   email: z.string(),
@@ -14,86 +15,88 @@ const UserLoginFormSchema = z.object({
 });
 
 export default function Login() {
-  const { control, handleSubmit } = useForm({
+  const { control, handleSubmit } = useForm<LoginRequest>({
     resolver: zodResolver(UserLoginFormSchema),
   });
   const { mutateAsync } = useUserLoginCreate();
   const [error, setError] = useState<string | null>(null);
 
+  const onSubmit = async (data: LoginRequest) => {
+    setError(null);
+    return mutateAsync({ data })
+      .then(() => window.location.assign("/profile"))
+
+      .catch((error) => {
+        if (!isAxiosError(error) || !(error instanceof Error)) {
+          setError(JSON.stringify(error, null, 2));
+          return;
+        }
+
+        if (!error.response) {
+          setError(error.message);
+          return;
+        }
+
+        if (error.response.data.detail) {
+          setError(error.response.data.detail);
+        } else {
+          setError(`Error: ${error.response.data}`);
+        }
+      });
+  };
+
   return (
-    <div className={"grid-cols-2 grid gap-8"}>
-      <Form
-        onSubmit={handleSubmit(async (data) => {
-          setError(null);
-          return mutateAsync({ data })
-            .then(() => window.location.assign("/profile"))
-
-            .catch((error) => {
-              if (!isAxiosError(error) || !(error instanceof Error)) {
-                setError(JSON.stringify(error, null, 2));
-                return;
-              }
-
-              if (!error.response) {
-                setError(error.message);
-                return;
-              }
-
-              if (error.response.data.detail) {
-                setError(error.response.data.detail);
-              } else {
-                setError(`Error: ${error.response.data}`);
-              }
-            });
-        })}
-      >
-        <div className={"flex flex-col gap-4 w-full"}>
-          <h2 className={"text-lg font-bold"}>Logg inn</h2>
-          <Controller
-            control={control}
-            name={"email"}
-            render={({ field }) => (
-              <Input
-                id="email"
-                isRequired
-                label="E-post"
-                type="email"
-                autoComplete={"username"}
-                {...field}
-              />
-            )}
-          />
-          <Controller
-            control={control}
-            name={"password"}
-            render={({ field }) => (
-              <Input
-                isRequired
-                label="Passord"
-                {...field}
-                type="password"
-                autoComplete={"password"}
-              />
-            )}
-          />
-          {error}
-          <Button className={"ml-auto"} type="submit">
-            Logg inn
-          </Button>
+    <section className={"w-full max-w-3xl bg-background p-4 rounded-lg shadow-lg mt-10"}>
+      <div className={"grid-cols-2 grid gap-8"}>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <div className={"flex flex-col gap-4 w-full"}>
+            <h2 className={"text-lg font-bold"}>Logg inn</h2>
+            <Controller
+              control={control}
+              name={"email"}
+              render={({ field }) => (
+                <Input
+                  id="email"
+                  isRequired
+                  label="E-post"
+                  type="email"
+                  autoComplete={"section-login username"}
+                  {...field}
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name={"password"}
+              render={({ field }) => (
+                <Input
+                  isRequired
+                  label="Passord"
+                  {...field}
+                  type="password"
+                  autoComplete={"section-login password"}
+                />
+              )}
+            />
+            {error}
+            <Button className={"ml-auto"} type="submit">
+              Logg inn
+            </Button>
+          </div>
+        </Form>
+        <div className={"space-y-4 flex flex-col justify-between"}>
+          <div className={"text-lg"}>...eller registrer deg</div>
+          <div className={""}>
+            En profil kan brukes til å personalisere din brukeropplevelse. En bruker er også
+            nødvendig for å administrere medlemskap.
+          </div>
+          <div className={"ml-auto"}>
+            <Button as={Link} href={"/register"}>
+              Registerer bruker
+            </Button>
+          </div>
         </div>
-      </Form>
-      <div className={"space-y-4 flex flex-col justify-between"}>
-        <div className={"text-lg"}>...eller registrer deg</div>
-        <div className={""}>
-          En profil kan brukes til å personalisere din brukeropplevelse. En bruker er også nødvendig
-          for å administrere medlemskap.
-        </div>{" "}
-        <Link href={"/register"}>
-          <Button className={"ml-auto"} href={"/register"}>
-            Registerer bruker
-          </Button>
-        </Link>
       </div>
-    </div>
+    </section>
   );
 }
