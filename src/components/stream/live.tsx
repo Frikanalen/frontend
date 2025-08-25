97b778e747b05ddaea913fd3f833d605c8fc47bc
Tore Sinding Bekkedal <toresbe@protonmail.com>
2025-08-25 02:09:45 +0200

    emergency hotfix: add outage message

--- src/components/stream/live.tsx
+++ src/components/stream/live.tsx
@@ -3,6 +3,7 @@ import dynamic from "next/dynamic";
 import { Card, CardBody } from "@heroui/react";
 import { ScheduleitemRead } from "@/generated/frikanalenDjangoAPI.schemas";
 import { MetadataCurrentAndNext } from "@/components/stream/metadataCurrentAndNext";
+import { Alert } from "@heroui/alert";
 
 const VideoPlayer = dynamic(() => import("@/components/stream/VideoPlayer"), {
   ssr: false,
@@ -12,7 +13,16 @@ export const Live = ({ schedule }: { schedule: ScheduleitemRead[] }) => {
   return (
     <div className="flex flex-col gap-4">
       <Card className={"bg-background text-primary rounded-lg"}>
-        <VideoPlayer title="Frikanalen direkte" src="https://frikanalen.no/stream/index.m3u8" />
+        <Alert color={"danger"} className={"aspect-video"}>
+          <div className={"max-w-xl prose dark:prose-invert prose-xl"}>
+            <h2>Strømbrudd</h2>
+            <p>
+              Frikanalen er rammet av strømbrudd i et maskinrom som dessverre gjør oss ute av stand
+              til å sende. Vi jobber på spreng med å komme tilbake.
+            </p>
+          </div>
+        </Alert>
+        {/*<VideoPlayer title="Frikanalen direkte" src="https://frikanalen.no/stream/index.m3u8" />*/}
         <CardBody>
           <p className={"p-2 text-foreground"}>
             <em>Medlemmet er selv ansvarlig for innholdet i sine sendinger.</em>
